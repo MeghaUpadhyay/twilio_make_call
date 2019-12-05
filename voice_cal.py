f2ca1491dccd70399e2a7e175afb4c2b3af28c52
Megha Upadhyay <upadhyaymegha08@gmail.com>
2019-12-05 21:18:54 +0530

    Update voice_cal.py

--- voice_cal.py
+++ voice_cal.py
@@ -11,7 +11,7 @@ def voice():
     resp = VoiceResponse()
 
     # Read a message aloud to the caller
-    resp.say("hello world!", voice='alice')
+    resp.say("Hi , Thanks for calling. How May I Help You?", voice='alice')
 
     return str(resp)
 
