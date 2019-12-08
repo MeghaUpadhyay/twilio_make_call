734c20c3a23b8b0dbc1e41465e603d189587a2eb
Megha Upadhyay <upadhyaymegha08@gmail.com>
2019-12-08 12:15:54 +0530

    Update voice_cal.py

--- voice_cal.py
+++ voice_cal.py
@@ -8,13 +8,13 @@ app = Flask(__name__)
 def voice():
     """Respond to incoming phone calls with a 'Hello world' message"""
     
-    city = request.values['FromCity']
+    #city = request.values['FromCity']
 
     # Start our TwiML response
     resp = VoiceResponse()
 
     # Read a message aloud to the caller
-    resp.say('Hi , Thanks for calling PayPal offline payment service, {}'.format(city), voice='alice')
+    resp.say("Hi , Thanks for calling PayPal offline payment service", voice='alice')
 
     resp.hangup()
 
